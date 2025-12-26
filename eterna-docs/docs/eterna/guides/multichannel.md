3d0f99d044404ad3407f4cf79dd4cd311347c871
Ruben <rubencronie@gmail.com>
2025-12-26 22:10:35 +0100

    First commit

---
+++ eterna-docs/docs/eterna/guides/multichannel.md
@@ -0,0 +1,5 @@
+---
+sidebar_position: 2
+---
+
+# Exporting multichannel files
---
+++ eterna-docs/docusaurus.config.ts
@@ -0,0 +1,132 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'etsuko.fm / docs',
+  tagline: '',
+  favicon: 'img/favicon.ico',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://www.etsuko.fm/',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'etsuko-fm', // Usually your GitHub org/user name.
+  projectName: 'etsuko-fm-docs', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          sidebarCollapsed: false,
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: 'etsuko.fm / docs',
+      logo: {
+        alt: 'etsuko.fm logo',
+        src: 'img/logo.png',
+      },
+      items: [
+        // {
+        //   href: 'https://github.com/etsuko-fm',
+        //   label: 'GitHub',
+        //   position: 'right',
+        // },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Eterna',
+              to: '/docs/eterna',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'llllllll',
+              href: 'https://llllllll.co/',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'http://github.com/etsuko-fm/eterna',
+            },
+            {
+              label: 'Instagram',
+              href: 'https://instagram.com/etsuko.fm',
+            },
+            {
+              label: 'YouTube',
+              href: 'https://www.youtube.com/@etsuko-fm',
+            },          
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} etsuko.fm`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
