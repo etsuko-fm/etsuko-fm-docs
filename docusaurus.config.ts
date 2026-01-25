d2b8fe287cc95dcfb430b92b694e7ee51a5ab55c
Ruben <rubencronie@gmail.com>
2026-01-25 10:36:13 +0100

    change base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,10 +17,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://www.etsuko.fm/',
+  url: 'https://etsuko-fm.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/etsuko-fm-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
